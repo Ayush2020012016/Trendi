4b408c8351d18735fafdba71e93360c1d83b0086
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-14 17:16:08 +0530

    added Trending Products

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const HomePage = () => {
+    const [searchBarClicked, setSearchBarClicked] = useState(false);
+
+    const handleClick = () => {
+        setSearchBarClicked(true);
+      };
   return (
     <div className="home-page">
     <div class="homeMasterContainer">
