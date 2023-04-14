1fff545a98a28011c3b42377984dde7341cfa052
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-14 15:32:23 +0530

    aded the generate products function in fakerdata

---
+++ src/utils/fakerData.js
@@ -0,0 +1,17 @@
+import {faker} from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
+
+export const generateProducts = (quantity) => {
+  const products = [];
+  for (let i = 0; i < quantity; i++) {
+    const product = {
+      id: uuidv4(),
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: faker.image.imageUrl(300,300,"clothes",true),
+      isWishlisted: false
+    };
+    products.push(product);
+  }
+  return products;
+};
