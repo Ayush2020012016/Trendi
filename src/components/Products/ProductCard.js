adac110e3aae543196c9d5df6f4db3e56af20646
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-04-14 17:38:39 +0530

    added product card along with classnames pacakage

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,9 +1,44 @@
-    import React from 'react'
-    
-    const ProductCard = () => {
-      return (
-        <div>ProductCard</div>
-      )
-    }
-    
-    export default ProductCard
+import React, { useState } from "react";
+import classNames from "classnames";
+import "./ProductCard.scss";
+import heart from "../../static/heart.svg";
+
+const ProductCard = ({
+  id,
+  name,
+  price,
+  image,
+  onWishlistToggle,
+  isWishlist,
+  home,
+}) => {
+  const [wishlist, setWishlist] = useState("false")
+  const handleWishlistToggle = () => {
+    onWishlistToggle(id);
+  };
+
+  return (
+    <a href={""} className="product-card">
+    <div className="">
+      <div className="product-image-container">
+        <img src={image} alt={name} />
+        {home ? null : (
+          <img
+            src={heart}
+            className={classNames("wishlist-button", {
+              active: isWishlist,
+            })}
+            onClick={handleWishlistToggle}
+          />
+        )}
+      </div>
+      <div className="product-details-container">
+        <div className="product-name">{name}</div>
+        <div className="product-price">{price}</div>
+      </div>
+    </div>
+    </a>
+  );
+};
+
+export default ProductCard;
